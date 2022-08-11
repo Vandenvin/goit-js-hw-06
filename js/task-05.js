46bf23e709e2659ac0438920e4a5aa8a80cb7ce5
Serhii Vandenvin <sevandenvin@gmail.com>
2022-08-11 19:10:30 +0300

    task-05 completed

---
+++ js/task-05.js
@@ -0,0 +1,18 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input),
+// підставляє його поточне значення в span#name - output.
+// Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+const inputForm = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputForm.addEventListener('imput', event => {
+  outputName.textContent = event.currentTarget.value;
+});
+
+// Если необходимо вставить имя целиком, а не побуквенно то используем "blur"
+// inputForm.addEventListener('blur', event => {
+//   outputName.textContent = event.currentTarget.value;
+// });
