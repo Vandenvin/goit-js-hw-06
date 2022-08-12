a0df1e98a27deee45c89c86e91086d9c59915c09
Serhii Vandenvin <sevandenvin@gmail.com>
2022-08-12 16:52:00 +0300

    task-08 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,52 @@
+// Напиши скрипт управління формою логіна.
 
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Login</button>
+// </form>
+
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те,
+// що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт,
+// де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості.
+// Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const form = document.querySelector('.login-form');
+
+// Google ругается что не стоит значение autocomplete, поэтому добавлены следующие 2 строчки кода
+const labelPassword = form.children[1].querySelector('input');
+labelPassword.autocomplete = 'off';
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formResult = {};
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  formResult.email = email.value;
+  formResult.password = password.value;
+
+  console.log(formResult);
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  event.currentTarget.reset();
+}
